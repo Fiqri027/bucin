8e44e0a52c5b3409f42c76a995393d1895964474
Fiqri027 <77881956+Fiqri027@users.noreply.github.com>
2021-04-01 21:52:06 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "", quotes: "Hopefully God will bestow health and happiness upon you." },
+    { author: "", quotes: "May all of your wishes will come true." },
+    { author: "", quotes: "May you always be beautiful and cheerful as always." },
+    { author: "", quotes: "All laughs and tears we share make me feel awesome." },
+    { author: "", quotes: "I Wish every single dreams of yours will come true." },
+    { author: "", quotes: "Be Happy, be Awesome, May peace be upon you." },
+    { author: "", quotes: "I hope you always happy and all your dream will come true." },
 ];
